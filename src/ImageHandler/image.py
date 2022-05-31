e925603f094199444b9d897d618c5e60927a3da5
hfoxwell <hfoxwell@kings.edu.au>
2022-05-31 11:52:41 +1000

    Fixed image type declaration

--- src/ImageHandler/image.py
+++ src/ImageHandler/image.py
@@ -18,7 +18,7 @@ class image:
     image_name:str = field(compare=True)
     image_path: str = field(compare=False)
     file_type:str = field(compare=False)
-    image_size = field(init=False)
+    image_size:int = field(init=False)
 
     def __post_init__(self):
         self.image_size = os.path.getsize(self.image_file)
