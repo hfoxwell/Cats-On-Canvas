5d532b8675557e1a4eaaafaef8ff234d6d1c614d
hfoxwell <hfoxwell@kings.edu.au>
2022-05-31 14:54:56 +1000

    Revert canvas_requests back to object based
    Upon investigating the API canvas has more than one method for upload.
    
    This meant that it is possible that another method will be more effective.
    
    Therefore, an abstract class was used to ensure that this was possible.

--- src/Requests/canvas_requests.py
+++ src/Requests/canvas_requests.py
@@ -7,6 +7,7 @@
 '''
 
 # External imports
+from abc import ABC, abstractmethod
 import requests, json, collections
 
 # Internal imports
@@ -15,59 +16,79 @@ from src.Logger.log import write_error, write_log
 from src.ImageHandler.image_handler import open_image
 
 
-# Module Functions
+class Canvas_connector(ABC):
+    '''Abstract base class for canvas connector'''
 
-''' For passing information to canvas '''
-settings = json.load(open(file='./Settings/settings.json', encoding='utf-8'))
-Auth_token: str = settings['access_token']
-domain: str = f'https://{settings["domain"]}/api/v1/users'
-header: str = ""
-params: tuple = {}
+    def __init__(self) -> None:
+        self.settings = json.load(open(file='./Settings/settings.json', encoding='utf-8'))
+        self.Auth_token: str = self.settings['access_token']
+        self.domain: str = f'https://{self.settings["domain"]}/api/v1/users'
+        self.header: str = ""
+        self.params: tuple = {}
 
-def get_canvas_user_id(user:client) -> bool:
-    ''' Gets a user ID from Canvas '''
+    @abstractmethod
+    def get_canvas_id(self, user:client) -> bool:
+        ''' Gets internal canvas ID from student ID in SIS. Returns bool (true) on success '''
+
+    @abstractmethod
+    def upload_user_data(self, user:client) -> bool:
+        ''' Uploads user data to canvas. Returns bool (true) on success'''
+
+class POST_data_canvas(Canvas_connector):
     
-    write_log(
-        f'USER: Getting Canvas ID for: {user.client_id}'
-    )
+    def __init__(self) -> None:
+        super().__init__()
+        # ''' For passing information to canvas '''
+        # settings = json.load(open(file='./Settings/settings.json', encoding='utf-8'))
+        # Auth_token: str = settings['access_token']
+        # domain: str = f'https://{settings["domain"]}/api/v1/users'
+        # header: str = ""
+        # params: tuple = {}
 
-    user_Details = requests.get(
-        f'{domain}/sis_user_id:{user.client_id}',
-        headers=header,
-        params=params
-    )
+    def get_canvas_user_id(self, user:client) -> bool:
+        ''' Gets a user ID from Canvas '''
+        
+        write_log(
+            f'USER: Getting Canvas ID for: {user.client_id}'
+        )
 
-    if 'id' in user_Details.json:
-        user.client_id = user_Details.json()['id']
-        return True
-    else:
-        write_error(f'USER: {user.client_id} cannot be found in canvas')
-        return False
+        user_Details = requests.get(
+            f'{self.domain}/sis_user_id:{user.client_id}',
+            headers=self.header,
+            params=self.params
+        )
 
-def upload_image(user: client) -> bool:
-    ''' Upload image to users files '''
-    # Variables
-    url: str = domain + '/self/files'
-    inform_parameters = {
-        'name':user.image.image_name,
-        'size':user.image.image_size, # read the filesize
-        'content_type':user.image.file_type,
-        'parent_folder_path':'profile pictures',
-        'as_user_id': user.client_id
-        }
+        if 'id' in user_Details.json:
+            user.client_id = user_Details.json()['id']
+            return True
+        else:
+            write_error(f'USER: {user.client_id} cannot be found in canvas')
+            return False
+
+    def upload_image(self, user: client) -> bool:
+        ''' Upload image to users files '''
+        # Variables
+        url: str = self.domain + '/self/files'
+        inform_parameters = {
+            'name':user.image.image_name,
+            'size':user.image.image_size, # read the filesize
+            'content_type':user.image.file_type,
+            'parent_folder_path':'profile pictures',
+            'as_user_id': user.client_id
+            }
+        
+        # Prepare Canvas for upload
+        responese = requests.post(url,headers=self.header,data=inform_parameters)
+
+        # Get response and send data
+        json_res = json.loads(responese.text,object_pairs_hook=collections.OrderedDict)
+
+        # Prepare data
+        files = user.image.image_file
+
+        _data = json_res.items()
+        _data[1] = ('upload_params',_data[1][1].items())
+
+        upload_file_response = requests.post(json_res['upload_url'],data=_data[1][1],files=files,allow_redirects=False)
     
-    # Prepare Canvas for upload
-    responese = requests.post(url,headers=header,data=inform_parameters)
-
-    # Get response and send data
-    json_res = json.loads(responese.text,object_pairs_hook=collections.OrderedDict)
-
-    # Prepare data
-    files = user.image.image_file
-
-    _data = json_res.items()
-    _data[1] = ('upload_params',_data[1][1].items())
-
-    upload_file_response = requests.post(json_res['upload_url'],data=_data[1][1],files=files,allow_redirects=False)
-   
 
