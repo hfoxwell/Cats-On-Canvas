8747343714bd1931f565c6ffd7c133c554231db1
hfoxwell <hfoxwell@kings.edu.au>
2022-05-31 10:54:24 +1000

    Changed Canvas_requests to a module This was done so that a singleton type pattern could be avoided.

--- src/Requests/canvas_requests.py
+++ src/Requests/canvas_requests.py
@@ -13,66 +13,63 @@ import requests, json, collections
 from src.Clients.user import client
 from src.Logger.log import logger
 
-# File Class
-class Canvas_requester():
-    ''' For passing information to canvas '''
 
-    def __init__(self, auth: str, domain: str, log:logger) -> None:
-        self.Auth_token: str = auth
-        self.domain: str = f'https://{domain}/api/v1/users'
-        self.header: str = ""
-        self.params: tuple = {}
+# Module Functions
 
-        # Import Logger
-        self.logger = log
+''' For passing information to canvas '''
+settings = json.load("./Settings/settings.json")
+Auth_token: str = settings['access_token']
+domain: str = f'https://{settings["domain"]}/api/v1/users'
+header: str = ""
+params: tuple = {}
+
+def get_canvas_user_id(user:client) -> bool:
+    ''' Gets a user ID from Canvas '''
     
-    def get_canvas_user_id(self, user:client) -> bool:
-        ''' Gets a user ID from Canvas '''
-        
-        logger.write_log(
-            f'USER: Getting Canvas ID for: {user.client_id}'
-        )
+    logger.write_log(
+        f'USER: Getting Canvas ID for: {user.client_id}'
+    )
 
-        user_Details = requests.get(
-            f'{self.domain}/sis_user_id:{user.client_id}',
-            headers=self.header,
-            params=self.params
-        )
+    user_Details = requests.get(
+        f'{domain}/sis_user_id:{user.client_id}',
+        headers=header,
+        params=params
+    )
 
-        if 'id' in user_Details.json:
-            user.client_id = user_Details.json()['id']
-            return True
-        else:
-            logger.write_error(f'USER: {user.client_id} cannot be found in canvas')
-            return False
+    if 'id' in user_Details.json:
+        user.client_id = user_Details.json()['id']
+        return True
+    else:
+        logger.write_error(f'USER: {user.client_id} cannot be found in canvas')
+        return False
 
-    def upload_image(self, user: client) -> bool:
-        ''' Upload image to users files '''
-        # Variables
-        url: str = self.domain + '/self/files'
-        inform_parameters = {
-            'name':user.image.image_name,
-            'size':user.image.image_size, # read the filesize
-            'content_type':user.image.file_type,
-            'parent_folder_path':'profile pictures',
-            'as_user_id': user.client_id
-            }
-        
-        # Prepare Canvas for upload
-        responese = requests.post(url,headers=self.header,data=inform_parameters)
+def upload_image(user: client) -> bool:
+    ''' Upload image to users files '''
+    # Variables
+    url: str = domain + '/self/files'
+    inform_parameters = {
+        'name':user.image.image_name,
+        'size':user.image.image_size, # read the filesize
+        'content_type':user.image.file_type,
+        'parent_folder_path':'profile pictures',
+        'as_user_id': user.client_id
+        }
+    
+    # Prepare Canvas for upload
+    responese = requests.post(url,headers=header,data=inform_parameters)
 
-        # Get response and send data
-        json_res = json.loads(responese.text,object_pairs_hook=collections.OrderedDict)
+    # Get response and send data
+    json_res = json.loads(responese.text,object_pairs_hook=collections.OrderedDict)
 
-        # Prepare data
-        files = user.image.image_file
+    # Prepare data
+    files = user.image.image_file
 
-        _data = json_res.items()
-        _data[1] = ('upload_params',_data[1][1].items())
+    _data = json_res.items()
+    _data[1] = ('upload_params',_data[1][1].items())
 
-        upload_file_response = requests.post(json_res['upload_url'],data=_data[1][1],files=files,allow_redirects=False)
-        ''' 
-        TODO: As the image is stored inside the user object. The user needs to be created with an image.
-        This is currently not done. So refactoring of the reader and user creation needed.  
-        '''
+    upload_file_response = requests.post(json_res['upload_url'],data=_data[1][1],files=files,allow_redirects=False)
+    ''' 
+    TODO: As the image is stored inside the user object. The user needs to be created with an image.
+    This is currently not done. So refactoring of the reader and user creation needed.  
+    '''
 
