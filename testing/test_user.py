ad3193be0a7e67eeaf37a2f4590d81b26322e53e
ClockFox <62626942+hfoxwell@users.noreply.github.com>
2022-06-19 11:15:41 +1000

    Test config & test user created
    two tests created for both test user and test config

--- testing/test_user.py
+++ testing/test_user.py
@@ -6,16 +6,40 @@
 '''
 # External imports
 import pytest
-import random
 from pytest import MonkeyPatch
 
+
 # Internal import
-from src.Clients import user
+from src.Clients.user import client
 from src.ImageHandler import image
 
 def test_client(monkeypatch: MonkeyPatch):
-    ''' Test creating client '''
+    ''' Test client Creation'''
+    # Provide function to patch image init
+    def patch_image():
+        return object()
 
+    # Monkey Patch the initaliser
+    monkeypatch.setattr(image,'image',  patch_image)
+
+    # Create new user
+    newuser = client(12345, image.image())
+
+    # New user should not be null 
+    #   and new user should have user id
+    assert newuser != None
+    assert newuser.client_id == 12345
+
+
+def test_no_client(monkeypatch: MonkeyPatch):
+    ''' Test client failure'''
     
+    # Provide function to patch image init
+    def patch_image():
+        return object()
 
-    u1 = user.client(random.randint(1, 10000), image)
+    # Monkey Patch the initaliser
+    monkeypatch.setattr(image,'image',  patch_image)
+
+    with pytest.raises(ValueError):
+        newuser = client("Hello", image.image())
