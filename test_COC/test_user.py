31e0dc25f73c081a762606cae52cd5ca984522d7
Hayden Foxwell <foxwellhayden@gmail.com>
2023-03-19 15:01:28 +1100

    Fix some monkeypatch issues for the test

--- test_COC/test_user.py
+++ test_COC/test_user.py
@@ -5,46 +5,82 @@
         Test the user class
 '''
 # External imports
-import pytest, random, string
+import pytest
+import random
+import string
 from pytest import MonkeyPatch
 
-
 # Internal import
-import src.Clients
-import src.Image
+from src.Clients import client
+from src.Image import image
 
-def test_client(monkeypatch: MonkeyPatch):
-    ''' Test client Creation'''
+
+# Provide function to patch image init
+def patch_image(x):
+        return None
+
+def test_client_creation_with_valid_input(monkeypatch: MonkeyPatch):
+    '''
+    Test client creation with valid input.
+    '''
     # Variables
     user_ID: str = "".join(random.choices(string.ascii_letters + string.digits, k=10))
 
-    # Provide function to patch image init
-    def patch_image():
-        return object()
+    
 
-    # Monkey Patch the initaliser
-    monkeypatch.setattr(image,'image',  patch_image)
+    # Monkey Patch the initializer
+    monkeypatch.setattr(image, '__init__', patch_image)
 
     # Create new user
-    newuser = client(user_ID, image.image())
+    newuser = client(user_ID, image())
 
     # New user should not be null 
     #   and new user should have user id
-    assert newuser != None
+    assert newuser is not None
     assert newuser.client_id == user_ID
+    assert isinstance(newuser.image, image)
 
 
-def test_no_client(monkeypatch: MonkeyPatch):
-    ''' Test client failure'''
+def test_client_creation_with_invalid_input(monkeypatch: MonkeyPatch):
+    '''
+    Test client creation with invalid input.
+    '''
+    # Variables
+    user_ID: str = ""
 
-    user_ID = ""                # Test empty client ID
-    
-    # Provide function to patch image init
-    def patch_image():
-        return object()
+    # Monkey Patch the initializer
+    monkeypatch.setattr(image, '__init__', patch_image)
 
-    # Monkey Patch the initaliser
-    monkeypatch.setattr(image,'image',  patch_image)
-
+    # Ensure ValueError is raised with empty client ID
     with pytest.raises(ValueError):
-        newuser = client(user_ID, image.image())
+        client(user_ID, image())
+
+
+def test_client_creation_with_none_image_object(monkeypatch: MonkeyPatch):
+    '''
+    Test client creation with None image object.
+    '''
+    # Variables
+    user_ID: str = "".join(random.choices(string.ascii_letters + string.digits, k=10))
+
+    # Monkey Patch the initializer
+    monkeypatch.setattr(image, '__init__', None)
+
+    # Ensure ValueError is raised with None image object
+    with pytest.raises(ValueError):
+        client(user_ID, image())
+
+
+def test_client_creation_with_non_image_object(monkeypatch: MonkeyPatch):
+    '''
+    Test client creation with non-image object.
+    '''
+    # Variables
+    user_ID: str = "".join(random.choices(string.ascii_letters + string.digits, k=10))
+
+    # Monkey Patch the initializer
+    monkeypatch.setattr(image, '__init__', patch_image)
+
+    # Ensure ValueError is raised with non-image object
+    with pytest.raises(ValueError):
+        client(user_ID, image())
