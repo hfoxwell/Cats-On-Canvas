08513d59d516c549e6bad1d984de251c6173a8cf
Hayden Foxwell <foxwellhayden@gmail.com>
2023-06-03 09:29:41 +1000

    Updated Custom error DirectoriesCheckError

--- src/custom_errors/errors.py
+++ src/custom_errors/errors.py
@@ -11,4 +11,6 @@
 ###############################
 
 class DirectoriesCheckError(Exception):
-    pass
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
