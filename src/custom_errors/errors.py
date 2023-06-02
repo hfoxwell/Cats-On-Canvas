d6a50347cdf037dff1b49b3d5fe98750943d61bf
Hayden Foxwell <foxwellhayden@gmail.com>
2023-06-03 09:26:13 +1000

    Added custom error types for file check error
    File checking should raise a custom error type when the file directory
    and contents of said directory are not valid.

---
+++ src/custom_errors/errors.py
@@ -0,0 +1,14 @@
+'''
+    Author:     Hayden Foxwell
+    Date:       03/06/2023
+    Purpose:
+        Custom error types which are used throughout 
+        the program
+'''
+
+###############################
+## File handling Errors
+###############################
+
+class DirectoriesCheckError(Exception):
+    pass
